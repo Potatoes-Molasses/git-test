0c0513672bddb6277291f8a6e40726bc26453d84
Candice <1253349565@qq.com>
2025-01-04 21:36:04 +0800

    hot-fix commit1

--- demo1/src/com/git/gitTest.java
+++ demo1/src/com/git/gitTest.java
@@ -6,5 +6,7 @@ public class gitTest {
         System.out.println("Hello Git2!");
         System.out.println("Hello Git3!");
         System.out.println("Hello Git4!");
+        System.out.println("Hello Git5!");
+
     }
 }
